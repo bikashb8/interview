363ee3c2acab82bf08644be1c40348445edb515b
bikash <bikashb8@hotmail.com>
2023-08-10 19:22:23 +0530

    Test

---
+++ test_0002.py
@@ -0,0 +1,6 @@
+#Python program to get the area of circle as example of sequential program
+import math
+r = float(input("Enter radius:"))
+area = math.pi*r**2
+print("Area of circle :",area)
+print("Area of circle = {:0.2f}".format((area)))
