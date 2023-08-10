e330f13f32fc9ced62762952de8341c8ef4b1bda
bikash <bikashb8@hotmail.com>
2023-08-10 20:33:07 +0530

    Test

---
+++ test_0006.py
@@ -0,0 +1,9 @@
+#Python program to know on if ... elif ... else
+#Python program to know whether the number is positive or negetive
+num = int(input("Enter the number:"))
+if num == 0:
+    print(num,"is zero")
+elif num >0:
+    print(num,"num is positive")
+else:
+    print(num,"num is negative")
